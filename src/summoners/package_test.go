f5123dcc361320c08cd8e4ad7977d9357ad85842
msoedov <msoedov@gmail.com>
2014-06-14 14:39:26 +0300

    Summoners

---
+++ src/summoners/package_test.go
@@ -0,0 +1,33 @@
+package summoners
+
+import ("testing"
+)
+
+
+func TestNewNamesBasic(t *testing.T){
+
+	var name string = NewName(0)
+	if name == ""{
+		t.Errorf("Expected non empty name")
+	}
+}
+
+
+func TestNewNamesCollision(t *testing.T){
+
+	var name_first string = NewName(0)
+	var name_after string = NewName(1)
+	if name_after == name_first {
+		t.Errorf("Unaccepted name generation")
+	}
+}
+
+
+func TestNewNamesGeneration(t *testing.T){
+	for i := 0; i < 5; i++ {
+		var name string = NewName(i)
+		if name == "" {
+			t.Errorf("Expected non empty name")
+		}
+	}
+}
---
+++ src/summoners/uid.go
@@ -0,0 +1,29 @@
+package summoners
+
+import (
+	"math/rand"
+	"time"
+)
+
+var randomSource = rand.New(rand.NewSource(int64(time.Now().Nanosecond())))
+
+
+func NewName(n int)(name string){
+	if n > namesVariationWidth{
+		panic("Out of my data sample!")
+	}
+	index := randInt(n*NamesRange, (n+1)*NamesRange)
+	return ChampionNames[index]
+}
+
+
+func randInt(min, max int) int {
+	if min == max {
+		return randInt(min, min+1)
+	}
+	if min > max {
+		return randInt(max, min)
+	}
+	n := randomSource.Int() % (max - min)
+	return n + min
+}
