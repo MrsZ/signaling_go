d3ce2ef5f73ece467e6aed31dbf494e836426042
Sasha Myasoedov <msoedov@gmail.com>
2014-06-16 12:15:13 +0300

    Martini Injector

--- src/signaling/app.go
+++ src/signaling/app.go
@@ -1,6 +1,5 @@
 package signaling
 
-import "net/http"
 import "github.com/go-martini/martini"
 import "github.com/martini-contrib/cors"
 
@@ -8,28 +7,22 @@ func App() *martini.ClassicMartini {
 	m := martini.Classic()
 	// Make a new Broker instance
 	broker := NewBroker()
+	m.Map(broker)
 
 	m.Get("/", func() string {
 		return "Sup"
 	})
-	// todo: ?broker injection
 	m.Use(cors.Allow(&cors.Options{
 		AllowOrigins: []string{"*"},
 		AllowMethods: []string{"POST", "OPTIONS"},
 		AllowHeaders: []string{"Origin", "Content-type"},
 	}))
 
-	m.Post("/update/:room",
-		func(resp http.ResponseWriter, req *http.Request, params martini.Params) {
-			UpdateHandler(resp, req, params, broker)
-		})
+	m.Post("/update/:room", UpdateHandler)
 
 	m.Options("/update/:room", OptionsHandler)
 
-	m.Get("/stream/:room",
-		func(resp http.ResponseWriter, req *http.Request, params martini.Params) {
-			ClientStream(resp, req, params, broker)
-		})
+	m.Get("/stream/:room", ClientStream)
 
 	return m
 }
