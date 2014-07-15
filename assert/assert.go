5917be311b5e01db25c69da81cb14d7f0baea77f
Sasha Myasoedov <msoedov@gmail.com>
2014-07-15 18:52:45 +0300

    Fix signature

--- assert/assert.go
+++ assert/assert.go
@@ -9,7 +9,7 @@ import (
 )
 
 // assert fails the test if the condition is false.
-func Assert(tb testing.T, condition bool, msg string, v ...interface{}) {
+func Assert(tb *testing.T, condition bool, msg string, v ...interface{}) {
 	if !condition {
 		_, file, line, _ := runtime.Caller(1)
 		fmt.Printf("\033[31m%s:%d: "+msg+"\033[39m\n\n", append([]interface{}{filepath.Base(file), line}, v...)...)
@@ -18,7 +18,7 @@ func Assert(tb testing.T, condition bool, msg string, v ...interface{}) {
 }
 
 // ok fails the test if an err is not nil.
-func Ok(tb testing.T, err error) {
+func Ok(tb *testing.T, err error) {
 	if err != nil {
 		_, file, line, _ := runtime.Caller(1)
 		fmt.Printf("\033[31m%s:%d: unexpected error: %s\033[39m\n\n", filepath.Base(file), line, err.Error())
@@ -27,7 +27,7 @@ func Ok(tb testing.T, err error) {
 }
 
 // equals fails the test if exp is not equal to act.
-func Equals(tb testing.T, exp, act interface{}) {
+func Equals(tb *testing.T, exp, act interface{}) {
 	if !reflect.DeepEqual(exp, act) {
 		_, file, line, _ := runtime.Caller(1)
 		fmt.Printf("\033[31m%s:%d:\n\n\texp: %#v\n\n\tgot: %#v\033[39m\n\n", filepath.Base(file), line, exp, act)
